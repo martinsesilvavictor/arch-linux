6dfc97227a04332ff8d87a77d770dce628718e8e
martinsesilvavictor <martinsesilvavictor@gmail.com>
2025-03-07 13:41:34 -0300

    [WIP] Start project

---
+++ src/ArchLinux/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using ArchLinux.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArchLinux.Controllers
+{
+    [Route("")]
+    [Route("arch-linux")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [Route("erro")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        
+    }
+}
