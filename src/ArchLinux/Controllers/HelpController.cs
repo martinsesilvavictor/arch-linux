15782fbbd2b6613968bf3656e92d0a9d1d60e0ef
martinsesilvavictor <martinsesilvavictor@gmail.com>
2025-03-07 13:48:14 -0300

    [Feature] Add new area

---
+++ src/ArchLinux/Controllers/HelpController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArchLinux.Controllers
+{
+    [Route("arch-linux/help")]
+    public class HelpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("instalacao")]
+        public IActionResult Instalacao() 
+        {
+            return View();
+        }
+        [Route("pos_instalacao")]
+        public IActionResult PosInstalacao() 
+        {
+            return View();
+        }
+    }
+}
